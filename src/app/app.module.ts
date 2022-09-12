cc1c4be1ee21d67979f46fd4eecd62e7974b559f
EmersonAlvaro <38971050+EmersonAlvaro@users.noreply.github.com>
2022-09-12 21:09:52 +0200

    6 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, MatIconModule ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, SlideshowComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
